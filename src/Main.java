fb77d24f39969f39791d4d37b9303e2942bfa84a
chris <drikosx323@gmail.com>
2024-03-12 11:58:44 +0200

    Added method reverseString using Stack

--- src/Main.java
+++ src/Main.java
@@ -2,51 +2,63 @@ import java.util.EmptyStackException;
 import java.util.Stack;
 
 public class Main {
-    public class Stack {
-        private int top;
-        private int[] arr;
+//    public class Stack {
+//        private int top;
+//        private int[] arr;
+//
+//        public Stack(int capacity) {
+//            top = -1;
+//            arr = new int[capacity];
+//        }
+//
+//        public Stack() {
+//            this(10);
+//        }
+//
+//        public void push(int data){
+//            if(isFull()){
+//                throw new RuntimeException("Stack is full");
+//            }
+//            top++;
+//            arr[top] = data;
+//        }
+//        public boolean isFull(){
+//            return arr.length == size();
+//        }
+//        public int size(){
+//            return top+1;
+//        }
+//
+//        public int pop(){
+//            if (isEmpty()){
+//                throw new RuntimeException("Stack is empty");
+//            }
+//            int result = arr[top];
+//            top--;
+//            return result;
+//        }
+//        public boolean isEmpty(){
+//            return top < 0;
+//        }
+//
+//        public int peek(){
+//            if (isEmpty()){
+//                throw new RuntimeException("Stack is empty");
+//            }
+//            return arr[top];
+//        }
+//    }
 
-        public Stack(int capacity) {
-            top = -1;
-            arr = new int[capacity];
+    public String reverseString(String s){
+        Stack<Character> stack = new Stack();
+        char[] chars = s.toCharArray();
+        for (char c : chars){
+            stack.push(c);
         }
-
-        public Stack() {
-            this(10);
+        for (int i=0; i<s.length(); i++){
+            chars[i] = stack.pop();
         }
-
-        public void push(int data){
-            if(isFull()){
-                throw new RuntimeException("Stack is full");
-            }
-            top++;
-            arr[top] = data;
-        }
-        public boolean isFull(){
-            return arr.length == size();
-        }
-        public int size(){
-            return top+1;
-        }
-
-        public int pop(){
-            if (isEmpty()){
-                throw new RuntimeException("Stack is empty");
-            }
-            int result = arr[top];
-            top--;
-            return result;
-        }
-        public boolean isEmpty(){
-            return top < 0;
-        }
-
-        public int peek(){
-            if (isEmpty()){
-                throw new RuntimeException("Stack is empty");
-            }
-            return arr[top];
-        }
+        return new String(chars);
     }
 
 
@@ -107,13 +119,16 @@ public class Main {
 //
 //        stack.pop();
 //        System.out.println(stack.peek());
-            Main main = new Main();
-            Main.Stack stack = main.new Stack(5);
-            stack.push(5);
-            stack.push(10);
-            stack.push(15);
+//            Main main = new Main();
+//            Main.Stack stack = main.new Stack(5);
+//            stack.push(5);
+//            stack.push(10);
+//            stack.push(15);
+//
+//            System.out.println(stack.peek());
+        Main sll = new Main();
 
-            System.out.println(stack.peek());
+        System.out.println(sll.reverseString("babhs"));
 
     }
 }
